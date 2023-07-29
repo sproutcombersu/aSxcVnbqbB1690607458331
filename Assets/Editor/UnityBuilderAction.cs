d09bb4764d8a85534f30f6607ca02dfede3726e0
EasyApk <easyapk@icloud.com>
2023-07-29 05:12:36 +0000

    update

--- Assets/Editor/UnityBuilderAction.cs
+++ Assets/Editor/UnityBuilderAction.cs
@@ -11,13 +11,13 @@ namespace UnityBuilderAction
     public static class BuildScript
     {
         private static readonly string Eol = Environment.NewLine;
-        private static string APP_NAME = "Test Saja";
-        private static string COMPANY_NAME = "EasyApk";
+        private static string APP_NAME = "Toilet Paint - Monster Color";
+        private static string COMPANY_NAME = "1690607547215623092937";
         private static string BUILD_VERSION_NAME = "1.0.0";
-        private static string PACKAGENAME = "com.janjijiwa.enakrasanya";
+        private static string PACKAGENAME = "com.skibidi.toilet.monster.painting";
         private static string PATH_KEYSTORE = "data.keystore";
         private static string KEYSTORE_PASS = "123456";
-        private static string KEY_ALIAS_NAME = "test";
+        private static string KEY_ALIAS_NAME = "PINJOL";
         private static string KEY_ALIAS_PASS = "123456";
 
         private static readonly string[] Secrets =
@@ -37,7 +37,6 @@ namespace UnityBuilderAction
             PlayerSettings.companyName = COMPANY_NAME;
             PlayerSettings.bundleVersion = BUILD_VERSION_NAME;
             PlayerSettings.SetApplicationIdentifier(BuildTargetGroup.Android, PACKAGENAME);
-
 
             // Apply build target
             var buildTarget = (BuildTarget)Enum.Parse(typeof(BuildTarget), options["buildTarget"]);
